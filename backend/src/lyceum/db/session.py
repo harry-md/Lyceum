e18571ce88d7aab65bb79094a2298cd580137523
harry-md <hai.maoduc@gmail.com>
2026-09-17 10:28:38 +0700

    update structure

---
+++ backend/src/lyceum/db/session.py
@@ -0,0 +1,23 @@
+from typing import AsyncIterator
+
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+from lyceum.core.config import get_settings
+
+settings = get_settings()
+
+engine = create_async_engine(
+    settings.database_url, echo=settings.sql_echo, pool_pre_ping=True
+)
+
+AsyncSessionFactory = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    autoflush=False,
+    expire_on_commit=False,
+)
+
+
+async def get_session() -> AsyncIterator[AsyncSession]:
+    async with AsyncSessionFactory() as session:
+        yield session
